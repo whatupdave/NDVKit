a32ed9c3cd05db764fed9cda402500d5329044c0
Nathan de Vries <nathan@atnan.com>
2011-05-03 12:11:26 +1000

    Use a unique context to identify whether we're interested in property change KVO notifications.

--- Classes/NDVKVOMacros.h
+++ Classes/NDVKVOMacros.h
@@ -7,19 +7,23 @@
 //
 
 
+static NSString* NDVPropertyChangeObserverContext = @"com.atnan.PropertyChangeObserver";
+
 #define OBSERVE_CHANGES_FOR_PROPERTIES(...) \
 \
 for (NSString* property in [NSArray arrayWithObjects:__VA_ARGS__, nil]) { \
   [self addObserver:self \
          forKeyPath:property \
             options:(NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld) \
-            context:NULL]; \
+            context:&NDVPropertyChangeObserverContext]; \
 }
 
 
 #define SYNTHESIZE_PROPERTY_CHANGE_OBSERVER() \
 \
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context { \
+  if (context != NDVPropertyChangeObserverContext) return [super observeValueForKeyPath:keyPath ofObject:object change:change context:context]; \
+\
   if ([[change valueForKey:NSKeyValueChangeKindKey] integerValue] == NSKeyValueChangeSetting) { \
     SEL changeSelector = NSSelectorFromString([keyPath stringByAppendingString:@"DidChange"]); \
     SEL changeSelectorWithValues = NSSelectorFromString([keyPath stringByAppendingString:@"DidChangeWithOldValue:newValue:"]); \
